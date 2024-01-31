bd99f7b9fab1435c28258baccf3d9b42c13d374f
Luan Valente <luanbvalente@hotmail.com>
2024-01-30 22:14:13 -0300

    Projeto Criado

---
+++ src/main/java/com/maisprati/BikeShop_API/BikeShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.maisprati.BikeShop_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maisprati/BikeShop_API/BikeShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maisprati.BikeShop_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6f34a50962127ce5eb207531be4aa0a26221798e
Luan Valente <luanbvalente@hotmail.com>
2024-01-30 22:16:43 -0300

    Bike Entity

---
+++ src/main/java/com/maisprati/BikeShop_API/entity/Bike.java
@@ -0,0 +1,98 @@
+package com.maisprati.BikeShop_API.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private String modelo;
+
+    private double preco;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCompra;
+
+    private String comprador;
+
+    private String loja;
+
+    public Bike() {
+    }
+
+    public Bike(String descricao, String modelo, double preco, LocalDate dataCompra, String comprador, String loja) {
+        this.descricao = descricao;
+        this.modelo = modelo;
+        this.preco = preco;
+        this.dataCompra = dataCompra;
+        this.comprador = comprador;
+        this.loja = loja;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(LocalDate dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public String getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(String comprador) {
+        this.comprador = comprador;
+    }
+
+    public String getLoja() {
+        return loja;
+    }
+
+    public void setLoja(String loja) {
+        this.loja = loja;
+    }
+}

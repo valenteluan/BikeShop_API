dbbf9194c8106578e6943bf98e8cf5ca2b77ce78
Luan Valente <luanbvalente@hotmail.com>
2024-02-06 18:06:52 -0300

    Spring Security - STATELESS

---
+++ src/main/java/com/maisprati/BikeShop_API/infra/SecurityConfigurations.java
@@ -0,0 +1,21 @@
+package com.maisprati.BikeShop_API.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}

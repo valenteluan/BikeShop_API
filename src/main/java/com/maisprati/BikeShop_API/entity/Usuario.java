045aa497ce5b88fe4ebe987e31a2dca8ded37f8c
Luan Valente <luanbvalente@hotmail.com>
2024-02-06 14:08:24 -0300

    Spring Security - Usuarios

---
+++ src/main/java/com/maisprati/BikeShop_API/entity/Usuario.java
@@ -0,0 +1,62 @@
+package com.maisprati.BikeShop_API.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String login, String senha) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Usuario usuario = (Usuario) object;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
